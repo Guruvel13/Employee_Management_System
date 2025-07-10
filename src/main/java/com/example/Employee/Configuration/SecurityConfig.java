010419f557c6a3ab3a83e49f5d057bf0fcdc18af
Guruvel13 <letters2guruvel@gmail.com>
2025-07-10 15:41:52 +0530

    DAY-5 AN

--- src/main/java/com/example/Employee/Configuration/SecurityConfig.java
+++ src/main/java/com/example/Employee/Configuration/SecurityConfig.java
@@ -2,19 +2,47 @@ package com.example.Employee.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetails() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails guru = User.builder()
+                .username("guru")
+                .password(passwordEncoder().encode("guru@13"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, guru);
     }
 }
