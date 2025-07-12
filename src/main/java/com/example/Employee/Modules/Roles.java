5241bc17b297e55335e5938951bc730983c4be88
Guruvel13 <letters2guruvel@gmail.com>
2025-07-12 09:22:04 +0530

    DAY-6 AN

--- src/main/java/com/example/Employee/Modules/Roles.java
+++ src/main/java/com/example/Employee/Modules/Roles.java
@@ -1,13 +1,21 @@
 package com.example.Employee.Modules;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Roles {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int roleId;
-    private  String roleName;
+    private int roleId;
+    private String roleName;
 }
--- src/main/java/com/example/Employee/Services/AuthService.java
+++ src/main/java/com/example/Employee/Services/AuthService.java
@@ -1,21 +1,24 @@
 package com.example.Employee.Services;
 
+
+import com.example.Employee.Modules.RegisterDetails;
 import com.example.Employee.Repository.RegisterRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.net.PasswordAuthentication;
 
 @Service
 public class AuthService {
 
     @Autowired
-    PasswordAuthentication passwordAuthentication;
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     RegisterRepo registerRepo;
 
-    public String AddNewEmployee(RegisterRepo registerRepo){
-        RegisterDe
+    public String addNewEmployee (RegisterDetails registerDetails){
+        RegisterDetails registerDetails1 = new RegisterDetails();
+
+        registerDetails1.setEmpId(registerDetails.getEmpId());
     }
 }
